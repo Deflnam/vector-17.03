40d906e8c7541bc0053710e03bf5fe4fbe17c6ae
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-03-22 20:40:17 +0300

    Add functions

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -2,6 +2,7 @@
 #define TOP_IT_VECTOR_HPP
 
 #include <cstddef>
+
 namespace topit
 {
   template< class T >
@@ -15,12 +16,12 @@ namespace topit
     Vector& operator=(const Vector&);
     Vector& operator=(Vector&&);
 
-    bool isEmpty() const noexcept; // чуть-чуть поменять
-    size_t getSize() const noexcept; // доделать
-    size_t getCapacity() const noexcept; // доделать
+    bool isEmpty() const noexcept; 
+    size_t getSize() const noexcept;
+    size_t getCapacity() const noexcept; 
 
-    void push_back(const T& value); // доделать
-    void popBack(); // доделать
+    void push_back(const T& value);
+    void popBack(); 
     void insert(size_t i, const T& v);
     void erase(size_t i);
 
@@ -48,13 +49,53 @@ topit::Vector< T >::~Vector()
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return !size_;
+  return size_ == 0;
+}
+
+template< class T >
+size_t topit::Vector< T >::getSize() const noexcept
+{
+  return size_;
+}
+
+template< class T >
+size_t topit::Vector< T >::getCapacity() const noexcept
+{
+  return cap_;
 }
 
 template< class T >
 void topit::Vector< T >::push_back(const T& value)
 {
+  if (size_ == cap_)
+  {
+    size_t new_cap = (cap_ == 0) ? 1 : cap_ * 2;
 
+    T* new_data = new T[new_cap];
+
+    for (size_t i = 0; i < size_; ++i)
+    {
+      new_data[i] = data_[i];
+    }
+
+    delete[] data_;
+
+    data_ = new_data;
+    cap_ = new_cap;
+  }
+
+  data_[size_] = value;
+  ++size_;
+}
+
+template< class T >
+void topit::Vector< T >::popBack()
+{
+  if (size_ > 0)
+  {
+    --size_;
+    data_[size_].~T();
+  }
 }
 
 #endif
